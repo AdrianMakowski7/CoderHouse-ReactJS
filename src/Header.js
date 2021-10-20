de3c0036ace94d919d485688edec25ca44e5d0dc
AMakowski <adrianlmr1994@gmail.com>
2021-10-19 22:32:24 -0300

    Entrega Desafío: Menú e-commerce

---
+++ src/Header.js
@@ -0,0 +1,19 @@
+const Header =() => {
+  return(
+    <header class="main-header">
+      <h1>Buscador de Ciudades</h1>
+      <nav>
+        <ul>
+          <li><a>Europa</a></li>
+          <li><a>America del Sur</a></li>
+          <li><a>Ameruca del Norte</a></li>
+          <li><a>Asia</a></li>
+          <li><a>Africa</a></li>
+          <li><a>Oceania</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
