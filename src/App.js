f516acae7a6c179c136cec334fdc93f5d7eba362
AMakowski <adrianlmr1994@gmail.com>
2021-10-26 20:43:22 -0300

    Entrega Desafío: Contador con botón

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import Header from "./Header"
+import {useState} from "react"
 
 const App =() => {
+
+
+  
+  const[contador , setContador]=useState(0)
+  const contadorCero = 0
+  
+  const sumarContador = () => {
+    setContador(contador + 1)
+  }
+  const restarContador = () => {
+    setContador(contador - 1)
+  }
+  const resetContador = () => {
+    setContador(contadorCero )
+  }
+
   return(
     <>
-      <Header/>
+      {<Header/>}
+      <div className="contador">
+      <button className="restaContador" onClick={restarContador} >Restar</button>
+      <button className="sumaContador" onClick={sumarContador} >Sumar</button>
+      <p>{contador}</p>
+      <button className="resetContador" onClick={resetContador} >Reset</button> 
+      </div>
     </>
   )
 }
